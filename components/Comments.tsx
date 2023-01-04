a567046f88005de1a13d6ef57b63500c273fe39e
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-01-05 00:45:37 +0700

    finished components: comment

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,10 +1,10 @@
 import React, { Dispatch, SetStateAction } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { GoVerified } from "react-icons/go";
 import useAuthStore from "../store/authStore";
 import NoResults from "./NoResults";
 import { IUser } from "../types";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
 
 interface IProps {
 	isPostingComment: Boolean;
@@ -28,13 +28,64 @@ const Comments = ({
 	addComment,
 	comments,
 }: IProps) => {
-	const { userProfile } = useAuthStore();
+	const { userProfile, allUsers } = useAuthStore();
 
 	return (
 		<div className="border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
 			<div className="overflow-scroll lg:h-[457px]">
 				{comments?.length ? (
-					<div>videps</div>
+					comments.map((item, index) => (
+						<>
+							{allUsers.map(
+								(user: IUser) =>
+									user._id ===
+										(item.postedBy._id ||
+											item.postedBy._ref) && (
+										<div
+											className=" p-2 items-center"
+											key={index}>
+											<Link href={`/profile/${user._id}`}>
+												<div className="flex items-start gap-3">
+													<div className="w-12 h-12">
+														<Image
+															width={34}
+															height={34}
+															className="rounded-full cursor-pointer"
+															src={user.image}
+															alt="user-profile"
+														/>
+													</div>
+
+													<div className="-ml-4 -mt-1">
+														<p className="flex cursor-pointer gap-1 items-center  font-bold leading-6 text-primary">
+															{user.userName
+																.toLocaleLowerCase()
+																.substring(
+																	0,
+																	16
+																)
+																.replace(
+																	/\s+/g,
+																	""
+																)}{" "}
+															<GoVerified className="text-blue-400" />
+														</p>
+														<p className="capitalize text-gray-400 text-xs">
+															{user.userName}
+														</p>
+													</div>
+												</div>
+											</Link>
+											<div>
+												<p className="-mt-2 mb-3 mr-8 capitalize">
+													{item.comment}
+												</p>
+											</div>
+										</div>
+									)
+							)}
+						</>
+					))
 				) : (
 					<NoResults text="No Comments Yet! Be First to do add the comment." />
 				)}
